c3629fe90b6b71fdfac2ba3472a09c535277c957
Rajarsi Saha <rajarsi3997@gmail.com>
2021-02-22 10:57:01 +0530

    Conducted Tests

--- src/Display/Display.test.tsx
+++ src/Display/Display.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import Display from './Display';
 
-test('Renders Conveter Section Card', () => {
+test('Renders Target Amount', () => {
   render(<Display conAmount="" />);
   expect(screen.getByText(/Target Amount/i))
+  .toBeInTheDocument();
+});
+
+test('Renders Desired Amount', () => {
+  render(<Display conAmount="" />);
+  expect(screen.getByTitle(/Desired Amount/i))
   .toBeInTheDocument();
 });
--- src/Form/Form.test.tsx
+++ src/Form/Form.test.tsx
@@ -1,14 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import Form from './Form';
 
-test('Renders Conveter Section Card', () => {
+test('Renders Base Amount', () => {
   render(<Form process="" state="" />);
   expect(screen.getByText(/Base Amount/i))
   .toBeInTheDocument();
+});
+test('Renders Base Currency', () => {
+  render(<Form process="" state="" />);
   expect(screen.getByText(/Base Currency/i))
   .toBeInTheDocument();
-  expect(screen.getByText(/Target Currency/i))
+});
+test('Renders Select Base Currency', () => {
+  render(<Form process="" state="" />);
+  expect(screen.getByTitle(/Select Base Currency/i))
   .toBeInTheDocument();
-  expect(screen.getByText(/Get Conversion/i))
-  .toBeInTheDocument();
+});
+test('Renders Target Currency', () => {
+  render(<Form process="" state="" />);
+expect(screen.getByText(/Target Currency/i))
+.toBeInTheDocument();
+});
+test('Renders Select Target Currency', () => {
+  render(<Form process="" state="" />);
+expect(screen.getByTitle(/Select Target Currency/i))
+.toBeInTheDocument();
+});
+test('Renders Get Conversion', () => {
+  render(<Form process="" state="" />);
+expect(screen.getByText(/Get Conversion/i))
+.toBeInTheDocument();
 });
