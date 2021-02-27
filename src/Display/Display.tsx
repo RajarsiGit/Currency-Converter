e78fb95bca91946c6d7d47ca197a6293017d130f
Rajarsi Saha <rajarsi3997@gmail.com>
2021-02-27 23:05:43 +0530

    Switched to Jumbotron for display

--- src/Display/Display.tsx
+++ src/Display/Display.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 //import styles from './Display.module.css';
-import { Container, Row, Col, Form, InputGroup } from 'react-bootstrap';
+import { Container, Row, Col, Jumbotron } from 'react-bootstrap';
 
 class Display extends React.Component<{conAmount: string}, {}> {
   constructor(props: any | Readonly<{}>) {
@@ -11,18 +11,20 @@ class Display extends React.Component<{conAmount: string}, {}> {
   render() {
     return (
       <Container fluid>
-        <Row className="justify-content-center pt-4 pb-5">
+        <Row className="justify-content-center pt-4 mb-5">
           <Col xs="12" lg="10">
-            <Form.Group controlId="Form.Display">
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text>
-                    Target Amount
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <Form.Control as="input" title="Desired Amount" readOnly className="text-right" value={this.props.conAmount}></Form.Control>
-              </InputGroup>
-            </Form.Group>
+            <Jumbotron>
+              <Container fluid>
+                <Row className="align-items-end justify-content-end">
+                  <Col xs="12" lg="5">
+                    <h4>Target Amount</h4>
+                  </Col>
+                  <Col xs="auto" lg="7" className="text-right">
+                    <h1 title="Desired Amount" className="display-4 font-weight-normal">{this.props.conAmount}</h1>
+                  </Col>
+                </Row>
+              </Container>
+            </Jumbotron>
           </Col>
         </Row>
       </Container>
