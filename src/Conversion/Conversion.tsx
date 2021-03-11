5e244f43aaffa4b55d228afd1ff4dc04bf6f33fc
Rajarsi Saha <rajarsi3997@gmail.com>
2021-03-11 09:44:09 +0530

    Modified

--- src/Conversion/Conversion.tsx
+++ src/Conversion/Conversion.tsx
@@ -1,46 +1,64 @@
-import { get } from 'https'
+import { request } from 'https'
+
+
+const getRate = (base: string, target: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    let q = base.toUpperCase() + target.toUpperCase();
+
+    const options1 = {
+      hostname: 'rocky-crag-10144.herokuapp.com',
+      port: 443,
+      path: '/https://www.freeforexapi.com/api/live?pairs=' + q,
+      method: 'GET'
+    }
+
+    const options2 = {
+      hostname: 'freeforexapi.com',
+      port: 443,
+      path: '/api/live?pairs=' + q,
+      method: 'GET'
+    }
+  
+    const req = request(options2, res => {
+      res.on('data', d => {
+        var jsonObj = JSON.parse(d);
+        var val = jsonObj['rates'][q].rate.toString();
+        resolve(val);
+      });
+    });
+  
+    req.on('error', (error: { message: any; }) => {
+      reject(error.message);
+    });
+  
+    req.end();
+  });
+}
 
 const Conversion = (callback: (...args: any[]) => void, baseCur: string, targetCur: string, amount: string) => {
-  var AP_KEY = '187c342ff3c5f3d02ed2';
-
-  baseCur = encodeURIComponent(baseCur);
-  targetCur = encodeURIComponent(targetCur);
-  var query = baseCur + '_' + targetCur;
-
-  var url = 'https://free.currconv.com/api/v7/convert?q=' + query + '&compact=ultra&apiKey=' + AP_KEY;
-
-  get(url, res => {
-    var body = '';
-
-    res.on('data', data => {
-      body += data;
-    });
-    
-    res.on('end', () => {
-      try {
-        var jsonObj = JSON.parse(body);
-        var val = jsonObj[query];
-        
-        if (val) {
-          var total = (Math.round(val * parseFloat(amount) * 10000) / 10000).toString();
-          var value = (Math.round(parseFloat(val) * 10000) / 10000).toString();
-          if (total.includes('.') && value.includes('.')) {
-            callback({amount: total, value: value});
-          } else if (value.includes('.')) {
-            callback({amount: total.concat('.0000'), value: value});
-          } else if (total.includes('.')) {
-            callback({amount: total, value: value.concat('.0000')});
-          } else {
-            callback({amount: total.concat('.0000'), value: value.concat('.0000')});
-          }
-        } else {
-          var err = new Error("Value not found for " + query);
-          console.log(err);
-        }
-      } catch(e) {
-        console.log("Parse error: ", e);
+  getRate('USD', baseCur)
+  .then((base) => {
+    getRate('USD', targetCur)
+    .then((target) => {
+      var val = 1/parseFloat(base) * parseFloat(target);
+      var total = (Math.round(val * parseFloat(amount) * 10000) / 10000).toString();
+      var value = (Math.round(parseFloat(val.toString()) * 10000) / 10000).toString();
+      if (total.includes('.') && value.includes('.')) {
+        callback({amount: total, value: value});
+      } else if (value.includes('.')) {
+        callback({amount: total.concat('.0000'), value: value});
+      } else if (total.includes('.')) {
+        callback({amount: total, value: value.concat('.0000')});
+      } else {
+        callback({amount: total.concat('.0000'), value: value.concat('.0000')});
       }
+    })
+    .catch((err) => {
+      console.log(err);
     });
+  })
+  .catch((err) => {
+    console.error(err);
   });
 }
 
